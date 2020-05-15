c14bdf57da6b78f2a1c67475f5a362fad1ef9583
lopdam <dennyslopez2013@gmail.com>
2020-05-15 11:50:46 -0500

    add full screen and time view

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,109 @@
-class Video{
-    constructor(selector){
-        this.selector=selector;
-        this.playerElement=document.querySelector(selector);
+class Video {
+	constructor(selector) {
+		this.selector = selector;
+		this.playerElement = document.querySelector(selector);
 
-        this.videoElement = document.querySelector(selector+" video");
+		this.videoElement = document.querySelector(selector + " video");
 
-        this.bindEvents();
+		this.bindEvents();
+	}
 
-    }
+	bindEvents() {
+		//Get Elements
+		this.playPauseBtn = document.querySelector(this.selector + " .play-pause");
+		this.showVolumeBtn = document.querySelector(
+			this.selector + " .show-volume"
+		);
+		this.volumeRange = document.querySelector(this.selector + " #volume-range");
+		this.progressBar = document.querySelector(this.selector + " .bar");
 
-    bindEvents(){
-        this.playPauseBtn=document.querySelector(this.selector+" .play-pause");
+		this.fullScreenBtn = document.querySelector(
+			this.selector + " .full-screen"
+		);
 
-        this.playPauseBtn.addEventListener("click",()=>this.playPause());
-    }
-    
-    playPause(){
+		//Events
+		this.playPauseBtn.addEventListener("click", () => this.playPause());
+		this.showVolumeBtn.addEventListener("click", () => this.toggleVolume());
 
-        if(this.videoElement.paused){
+		this.videoElement.addEventListener("timeupdate", () =>
+			this.updateProgress()
+		);
 
-            this.playPauseBtn.innerHTML="pause";
-            this.videoElement.play();
-        }
-        else{
-            this.playPauseBtn.innerHTML = "play_arrow";
-            this.videoElement.pause();
-        }
-    }
+		this.videoElement.addEventListener("durationchange", () =>
+			this.setFullDuration()
+		);
+
+		this.volumeRange.addEventListener("input", () => this.updateVolume());
+
+		this.fullScreenBtn.addEventListener("click", () => this.fullScreenVideo());
+	}
+
+	playPause() {
+		if (this.videoElement.paused) {
+			this.playPauseBtn.innerHTML = "pause";
+			this.videoElement.play();
+		} else {
+			this.playPauseBtn.innerHTML = "play_arrow";
+			this.videoElement.pause();
+		}
+	}
+
+	toggleVolume() {
+		document
+			.querySelector(this.selector + " .volume")
+			.classList.toggle("active");
+	}
+
+	updateVolume() {
+		this.videoElement.volume = this.volumeRange.value;
+
+		if (this.volumeRange.value == 0) {
+			this.showVolumeBtn.innerHTML = "volume_off";
+		} else if (this.volumeRange.value <= 0.5) {
+			this.showVolumeBtn.innerHTML = "volume_down";
+		} else {
+			this.showVolumeBtn.innerHTML = "volume_up";
+		}
+	}
+
+	updateProgress() {
+		var videoDuration = this.videoElement.duration;
+		var videoPosition = this.videoElement.currentTime;
+
+		let videoPercent = (videoPosition / videoDuration) * 100;
+
+		this.progressBar.style.width = videoPercent.toString(10) + "%";
+
+		//set time actual
+
+		this.setDuration(videoPosition, ".time-actual");
+	}
+
+	setFullDuration() {
+		let durationTime = this.videoElement.duration;
+
+		this.setDuration(durationTime, ".full-duration");
+	}
+
+	setDuration(durationTime, selector) {
+		let minutes = parseInt(durationTime / 60, 10);
+
+		let seconds = parseInt(durationTime % 60, 10);
+
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		document.querySelector(this.selector + " " + selector).innerHTML =
+			minutes + ":" + seconds;
+	}
+
+	fullScreenVideo() {
+		let fullScreenFuntion =
+			this.videoElement.requestFullscreen ||
+			this.videoElement.webkitRequestFullScreen ||
+			this.videoElement.mozRequestFullScreen ||
+            this.videoElement.msRequestFullscreen;
+            
+            fullScreenFuntion.call(this.videoElement);
+	}
 }
